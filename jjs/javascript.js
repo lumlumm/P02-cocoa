ba8bb05464289ee9968d6e0423b79e10fa9408db
Unknown <mr_death_44@hotmail.com>
2017-06-14 01:29:49 -0700

    made all the pages except 2, gunna finish dat tmr, also changed some css

--- jjs/javascript.js
+++ jjs/javascript.js
@@ -1,6 +1,6 @@
 /*https://www.w3schools.com/w3css/w3css_tabulators.asp*/
 
-function openCity(evt, cityName) {
+function openEvent(evt, eventName) {
   var i, x, tablinks;
   x = document.getElementsByClassName("event_type");
   for (i = 0; i < x.length; i++) {
@@ -10,6 +10,20 @@ function openCity(evt, cityName) {
   for (i = 0; i < x.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(eventName).style.display = "block";
+  evt.currentTarget.className += " active_tab";
+}
+
+function openFood(evt, foodName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("food_type");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active_tab", "");
+  }
+  document.getElementById(foodName).style.display = "block";
   evt.currentTarget.className += " active_tab";
 }
